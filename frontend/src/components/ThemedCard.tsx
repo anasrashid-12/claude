abff4ad858bbda884d362a58c25ea80d7325ddbe
anasrashid-12 <m.anasrashid12@gmail.com>
2025-07-22 20:45:50 +0500

    Ready for deploy

--- frontend/src/components/ThemedCard.tsx
+++ frontend/src/components/ThemedCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Card } from '@shopify/polaris';
-import { cn } from '../lib/utils';
+import { cn } from '../../utils/utils';
 
 export default function ThemedCard({
   children,
