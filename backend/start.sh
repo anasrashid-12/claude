0d4abcb3e7748bc53ce8d28bf102fe477adf5603
anasrashid-12 <m.anasrashid12@gmail.com>
2025-07-27 15:31:43 +0500

    Ready for deploy

--- backend/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Celery in background
-celery -A app.celery_app.celery_app worker --loglevel=info -Q image_queue &
-
-# Start FastAPI server
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
