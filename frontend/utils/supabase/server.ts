c30cb3c9b71c1dcf7f6acf2b92b54b650ce3f3f8
anasrashid-12 <m.anasrashid12@gmail.com>
2025-07-31 02:13:09 +0500

    Ready for deploy

--- frontend/utils/supabase/server.ts
+++ frontend/utils/supabase/server.ts
@@ -4,6 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; 
 
+console.log('SERVICE ROLE KEY:', process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 export const supabaseServer = createClient(supabaseUrl, serviceRoleKey, {
   global: {
     headers: {
